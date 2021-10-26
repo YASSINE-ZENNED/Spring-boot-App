1535d7a3c08c50702ea97e331b91122af5aa458b
yassine <yassine.zenned.123@gmail.com>
2021-10-26 22:03:37 +0100

    email  send first commit

---
+++ src/main/java/com/example/demo/email/MyConstants.java
@@ -0,0 +1,14 @@
+package com.example.demo.email;
+
+public class MyConstants {
+
+    // Replace with your email here:
+    public static final String MY_EMAIL = "yassineZ@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "yourPassword";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "baya.hisham@gmail.com\n";
+
+}
